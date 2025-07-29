2cf44051496f2faefc67752eca54abcf2406bf36
Alan-Balvin <tareauasbalvin@gmail.com>
2025-07-29 12:28:06 -0700

    styling titles using flipwords

--- src/components/HeroText.tsx
+++ src/components/HeroText.tsx
@@ -7,9 +7,9 @@ const HeroText = () => {
         <div className="flex-col hidden md:flex c-space" >
           <h1 className="text-4xl font-medium">Hi I am Eduardo</h1> 
           <div className="flex flex-col items-start">
-            <p className="text-5xl font-medium text-neutral-300" >An Architect dedicated to model electrical BIM</p>
-<div><FlipWords words={["BIM", "Electrical", "Systems"]} className="font-black text-white" /></div>
-            <p className="text-4xl font-medium text-neutral-300" >Electrical Solutions</p>
+            <p className="text-5xl font-medium text-neutral-300" >An Architect dedicated to model</p>
+<div><FlipWords words={["BIM", "Electrical", "Systems"]} className="font-black text-white text-7xl" /></div>
+            <p className="text-4xl font-medium text-neutral-300" >Solutions</p>
 
             </div> 
         </div>
