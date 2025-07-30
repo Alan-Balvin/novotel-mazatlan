6bc0b078ca1e8f8961120130d606308d2c2807f6
Alan-Balvin <tareauasbalvin@gmail.com>
2025-07-30 10:33:48 -0700

    updating motion

--- src/components/HeroText.tsx
+++ src/components/HeroText.tsx
@@ -1,25 +1,26 @@
 import { FlipWords } from "./FlipWords"
+import { motion } from "motion/react" 
 const words= ["BIM", "Electrical", "Systems"];
 const HeroText = () => {
   return (
     <div className="z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text">
         {/*DesktopView */}
         <div className="flex-col hidden md:flex c-space" >
-          <h1 className="text-4xl font-medium">Hi I am Eduardo</h1> 
-          <div className="flex flex-col items-start">
-            <p className="text-5xl font-medium text-neutral-300" >An Architect specialized in modeling</p>
-<div><FlipWords words={words} className="font-black text-white text-7xl" /></div>
-            <p className="text-4xl font-medium text-neutral-300" >Solutions</p>
+          <motion.h1 className="text-4xl font-medium">Hi I am Eduardo</motion.h1> 
+          <motion.div className="flex flex-col items-start">
+            <motion.p className="text-5xl font-medium text-neutral-300" >An Architect specialized in modeling</motion.p>
+<motion.div><FlipWords words={words} className="font-black text-white text-7xl" /></motion.div>
+            <motion.p className="text-4xl font-medium text-neutral-300" >Solutions</motion.p>
 
-            </div> 
+            </motion.div> 
         </div>
         {/*Mobile View*/}
       <div className="flex- flex-col space-y-6 md:hidden" >
-        <p className="text-4xl font-medium" >Hi </p>
+        <motion.p className="text-4xl font-medium" >Hi </motion.p>
         <div>
-          <p className="text-5xl font-black text-neutral-300">Building</p>
-          <div><FlipWords words={words} className="font-bold text-white text-7xl"/></div>
-          <p className="text-4xl font-black text-neutral-300">Electrical Applications</p>
+          <motion.p className="text-5xl font-black text-neutral-300">Building</motion.p>
+          <motion.div><FlipWords words={words} className="font-bold text-white text-7xl"/></motion.div>
+          <motion.p className="text-4xl font-black text-neutral-300">Electrical Applications</motion.p>
         </div>
       </div>
     </div>
