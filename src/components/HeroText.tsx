dc40266bc957738eeaa655d4aa9184c08324127c
Alan-Balvin <tareauasbalvin@gmail.com>
2025-08-01 15:24:52 -0700

    adding functionality with motion

--- src/components/HeroText.tsx
+++ src/components/HeroText.tsx
@@ -1,14 +1,30 @@
 import { FlipWords } from "./FlipWords"
 import { motion } from "motion/react" 
+
 const words= ["BIM", "Electrical", "Systems"];
+
+
+
 const HeroText = () => {
+  const variants = {
+    hidden:{opacity:0, x:-200},
+    visible:{opacity:1, x:0},
+   }
   return (
     <div className="z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text">
         {/*DesktopView */}
         <div className="flex-col hidden md:flex c-space" >
-          <motion.h1 className="text-4xl font-medium">Hi I am Eduardo</motion.h1> 
+          <motion.h1 className="text-4xl font-medium"
+          variants={variants}
+          initial="hidden"
+          animate="visible"
+          transition={{delay:0.5}}>Hi I am Eduardo</motion.h1> 
           <motion.div className="flex flex-col items-start">
-            <motion.p className="text-5xl font-medium text-neutral-300" >An Architect specialized in modeling</motion.p>
+            <motion.p className="text-5xl font-medium text-neutral-300"
+          variants={variants}
+          initial="hidden"
+          animate="visible"
+          transition={{delay:0.8}} >An Architect specialized in modeling</motion.p>
 <motion.div><FlipWords words={words} className="font-black text-white text-7xl" /></motion.div>
             <motion.p className="text-4xl font-medium text-neutral-300" >Solutions</motion.p>
 
