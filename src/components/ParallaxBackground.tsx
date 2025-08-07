93fb98317ea4b1ed56bf8e080651490a76861f2e
Alan-Balvin <tareauasbalvin@gmail.com>
2025-08-06 17:15:03 -0700

    completing backgrounds images

--- src/components/ParallaxBackground.tsx
+++ src/components/ParallaxBackground.tsx
@@ -4,15 +4,15 @@ import { motion, useScroll, useTransform } from 'framer-motion'
 const ParallaxBackground = () => {
   const {scrollYProgress} = useScroll();
   const conduits3Y = useTransform(scrollYProgress, [0,0.5], ["0%","40%"]);
-  const planetsX = useTransform(scrollYProgress, [0,0.5], ["0%","-20%"]);
+  const planetsX = useTransform(scrollYProgress, [0,0.5], ["0%","60%"]);
   const mountain2Y = useTransform(scrollYProgress, [0,0.5], ["0%","30%"]);
   const mountain1Y = useTransform(scrollYProgress, [0,0.5], ["0%","0%"]);
   return (
     <section className='absolute inset-0 bg-black/40' >
 
-    <div className='relative h-screen overflow-y-hidden'>
+    <div className='relative h-screen overflow-y-hidden '>
         {/*Background sky*/}
-      <div className='absolute inset-0 w-full h-screen'
+      <div className='absolute inset-0 w-full h-screen '
       style={{
         backgroundImage:"url(/assets/sky.jpg)",
         backgroundPosition:"bottom",
@@ -23,6 +23,27 @@ const ParallaxBackground = () => {
         backgroundPosition:"bottom",
         backgroundSize:"cover",
         y: conduits3Y,}}
+       />
+       <motion.div className='absolute inset-0 '
+      style={{
+        backgroundImage:"url(/assets/planets.png)",
+        backgroundPosition:"bottom",
+        backgroundSize:"cover",
+        x: planetsX,}}
+       />
+       <motion.div className='absolute inset-0 '
+      style={{
+        backgroundImage:"url(/assets/mountain-2.png)",
+        backgroundPosition:"bottom",
+        backgroundSize:"cover",
+        y: mountain2Y,}}
+       />
+       <motion.div className='absolute inset-0 '
+      style={{
+        backgroundImage:"url(/assets/mountain-1.png)",
+        backgroundPosition:"bottom",
+        backgroundSize:"cover",
+        y: mountain1Y,}}
        />
 
       <div/>
