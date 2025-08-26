dc3586ab38e501d67af57febdbcd8f352babdb99
Alan-Balvin <tareauasbalvin@gmail.com>
2025-08-26 15:59:46 -0700

    styling TimeLine component

--- src/components/TimeLine.tsx
+++ src/components/TimeLine.tsx
@@ -42,27 +42,35 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
       className="c-space section-spacing"
       ref={containerRef}
     >
-     <h2>My Work Experience</h2>
-      <div ref={ref} className="relative max-w-7xl mx-auto pb-20">
+     <h2 className="text-heading">My Work Experience</h2>
+      <div ref={ref} className="relative pb-20">
         {data.map((item, index) => (
           <div
             key={index}
             className="flex justify-start pt-10 md:pt-40 md:gap-10"
           >
             <div className="sticky flex flex-col md:flex-row z-40 items-center top-40 self-start max-w-xs lg:max-w-sm md:w-full">
-              <div className="h-10 absolute left-3 md:left-3 w-10 rounded-full bg-white dark:bg-black flex items-center justify-center">
-                <div className="h-4 w-4 rounded-full bg-neutral-200 dark:bg-neutral-800 border border-neutral-300 dark:border-neutral-700 p-2" />
+              <div className="h-10 absolute -left-[15px] w-10 rounded-full
+               bg-midnight flex items-center justify-center">
+                <div className="h-4 w-4 rounded-full 
+                 bg-neutral-800 border  border-neutral-700 p-2" />
               </div>
-              <h3 className="hidden md:block text-xl md:pl-20 md:text-5xl font-bold text-neutral-500 dark:text-neutral-500 ">
-                {item.title}
-              </h3>
+              <div className="flex-col hidden gap-2 text-xl font-bold md:flex md:pl-20 md:text-4xl text-neutral-300 ">
+                <h3>{item.date}</h3>
+                <h3 className="text-3xl text-neutral-400">{item.title}</h3>
+                <h3 className="text-3xl text-neutral-500">{item.job}</h3>
+              </div>
             </div>
 
             <div className="relative pl-20 pr-4 md:pl-4 w-full">
-              <h3 className="md:hidden block text-2xl mb-4 text-left font-bold text-neutral-500 dark:text-neutral-500">
-                {item.title}
-              </h3>
-              {item.contents}{" "}
+              <div className="block mb-4 text-2xl font-bold text-left text-neutral-300 md:hidden">
+                <h3>{item.date}</h3>
+                <h3>{item.job}</h3>
+              </div>
+              {item.contents.map((content, index)=>(
+                  <p className="mb-3 font-normal text-neutral-400" key={index}>
+                    {content}</p>
+              ))}
             </div>
           </div>
         ))}
